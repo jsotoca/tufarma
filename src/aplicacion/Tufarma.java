2fb341a31c1defc22a44451d4ff32006e71ef2f2
jsotoca <jsotoca@unprg.edu.pe>
2021-02-16 01:27:30 -0500

    Configuracion inicial

---
+++ src/aplicacion/Tufarma.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion;
+
+/**
+ *
+ * @author Juan Antonio Soto Cabrera <https://github.com/jsotoca/>
+ */
+public class Tufarma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/configuracion/Entorno.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package configuracion;
+
+/**
+ *
+ * @author Juan Antonio Soto Cabrera <https://github.com/jsotoca/>
+ */
+public class Entorno {
+    public static String nombre = "Antonio";
+}
