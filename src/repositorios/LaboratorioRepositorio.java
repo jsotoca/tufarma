a26f510cc3fe34382f30856855c91dc798dfee9b
jsotoca <jsotoca@unprg.edu.pe>
2021-02-17 15:53:24 -0500

    Add labotatorio repository

---
+++ src/repositorios/LaboratorioRepositorio.java
@@ -0,0 +1,32 @@
+package repositorios;
+
+import configuracion.MySQL;
+import entidades.Laboratorio;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Juan Antonio Soto Cabrera <https://github.com/jsotoca/>
+ */
+public class LaboratorioRepositorio {
+    public static List<Laboratorio> getLaboratorios(){
+        List<Laboratorio> laboratorios = new ArrayList<>();
+        try {
+            
+            ResultSet rs = MySQL.executeQuery("SELECT * FROM products", null);
+            while(rs.next()) {
+                Laboratorio laboratorio = new Laboratorio();
+                laboratorio.setCodigo(rs.getInt(1));
+                laboratorio.setNombre(rs.getString(2));
+                laboratorio.setVigente(rs.getBoolean(3));
+                laboratorios.add(laboratorio);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return laboratorios;
+    }
+}
